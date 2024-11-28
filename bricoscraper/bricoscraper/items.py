2d2473ec0207f4222089284e6b2454cc11bf0d93
Ludivine.RB <ludivine.raby@gmail.com>
2024-11-28 12:01:26 +0100

    spider_produit fonctionnelle

--- bricoscraper/bricoscraper/items.py
+++ bricoscraper/bricoscraper/items.py
@@ -17,3 +17,15 @@ class CategorieItem(scrapy.Item):
     sous_sous_categorie = scrapy.Field()
     id_sous_sous_categorie = scrapy.Field()
     lien_sous_sous_categorie = scrapy.Field()
+
+class ProduitItem(scrapy.Item):
+    nom = scrapy.Field()
+    prix = scrapy.Field()
+    remise = scrapy.Field()
+    code = scrapy.Field()
+    EAN_13 = scrapy.Field()
+    reference_fabricant = scrapy.Field()
+    description = scrapy.Field()
+    categorie = scrapy.Field()
+    url_produit = scrapy.Field()
+    marque = scrapy.Field()
--- bricoscraper/bricoscraper/spiders/produit.py
+++ bricoscraper/bricoscraper/spiders/produit.py
@@ -1,10 +1,10 @@
 import scrapy
-
+from bricoscraper.items import ProduitItem
 
 class ProduitSpider(scrapy.Spider):
     name = "produit"
     allowed_domains = ["www.centrale-brico.com"]
-    start_urls = ["https://www.centrale-brico.com"]
+    start_urls = ["https://www.centrale-brico.com/electroportatif/equipement-stationnaire/accessoires-compresseur"]
 
     def parse(self, response):
 
@@ -14,27 +14,42 @@ class ProduitSpider(scrapy.Spider):
             produit_url=produit.css('h2.product-miniature-title a::attr(href)').get()
             #yield response.follow(produit_url, callback=self.parse_produits_parse)
             
-            yield response.follow(produit_url, callback=self.parse_produit_page)
+            yield response.follow(produit_url, callback=self.parse_produit_page, meta={
+                 "produit_url" : produit_url
+            })
+
+        suivant = response.xpath('//a[@rel="next"]/@href').get()
+        if suivant is not None:
+             yield response.follow(suivant, callback=self.parse)
+             
 
 
     def parse_produit_page(self, response):
             produit = response.css('div.container-fluid.no-padding')
-            prix = produit.css('span.the_price ::attr(content)').get()
+            prix_produit = produit.css('span.the_price ::attr(content)').get()
             nom_produit = produit.css('header.page-header h1::text').get()
-            remise = produit.css('span.text-red ::text').get()
-            code = produit.xpath("//span[@id='product-reference']/span[@itemprop='sku']/text()").get() 
+            remise_produit = produit.css('span.text-red ::text').get()
+            code_produit = produit.xpath("//span[@id='product-reference']/span[@itemprop='sku']/text()").get() 
             ean13 = produit.xpath("//span[@id='product-ean13']/text()").get()
             ref_fabricant = produit.xpath("//span[@id='product-manufacturer-reference']/text()").get() 
-            description = produit.css('#product-description p::text').get()
-            yield {
-                 'nom': nom_produit,
-                 'prix': prix,
-                 'remise': remise,
-                 'code': code,
-                 'EAN 13': ean13,
-                 'référence fabricant' : ref_fabricant,
-                 'description' : description
-                 }
+            description_produit = produit.css('#product-description p::text').get()
+            categorie_produit = produit.css('ul.breadcrumb li')[3].css('a span::text').get()
+            marque_produit = response.css('img.product-manufacturer-thumbnail.img-fluid.d-block.ml-auto::attr(alt)').get()
+
+            yield ProduitItem(
+                 nom = nom_produit,
+                 prix = prix_produit,
+                 remise = remise_produit,
+                 code = code_produit,
+                 EAN_13 = ean13,
+                 reference_fabricant = ref_fabricant,
+                 description = description_produit,
+                 categorie = categorie_produit,
+                 url_produit = response.meta["produit_url"],
+                 marque = marque_produit
+            )
+
+            
                 #récupère toutes les infos d'un produit sans passer à la page suivante
 
     #def parse_produit_page(self, response):
